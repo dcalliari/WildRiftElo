23b88e4011313b30916e2137ee1ec075a53bb491
booude <booude@gmail.com>
2022-05-24 11:29:35 -0300

    declaring lang variable

--- static/python/bot.py
+++ static/python/bot.py
@@ -108,13 +108,14 @@ class Bot(commands.Bot):
     async def command_elo(self, ctx: commands.Context):
         canal = ctx.channel.name
         channel = ctx.message.content.split(' ')[1:]
+        lang = mod.lang()[await mod.get_lang(canal)]
         if channel != []:
             canal = channel[0]
         else:
             pass
         if canal != 'loraakl':
             try:
-                lang = mod.lang()[await mod.get_lang(canal)]['elo']
+                lang = lang['elo']
             except AttributeError:
                 await ctx.reply(lang['not_found'])
             id = ctx.message.content.split(' ', 1)[0][-1]
@@ -144,13 +145,14 @@ class Bot(commands.Bot):
     async def command_elowr(self, ctx: commands.Context):
         canal = ctx.channel.name
         channel = ctx.message.content.split(' ')[1:]
+        lang = mod.lang()[await mod.get_lang(canal)]
         if channel != []:
             canal = channel[0]
         else:
             pass
         if canal == 'loraakl':
             try:
-                lang = mod.lang()[await mod.get_lang(canal)]['elo']
+                lang = lang['elo']
             except AttributeError:
                 await ctx.reply(lang['not_found'])
             id = ctx.message.content.split(' ', 1)[0][-1]
